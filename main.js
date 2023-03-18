2c5ec2ecf64c11ce2a38fde37ff4d1abf9549b9f
Adrian <126387264+adrn-en@users.noreply.github.com>
2023-03-18 18:24:27 +0800

    Add files via upload
    initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const margin = document.getElementById('margin'),
+	leverage = document.getElementById('leverage'),
+	takeprofit = document.getElementById('tp'),
+	stoploss = document.getElementById('sl'),
+	capital_risk = document.getElementById('risk'),
+	inputs = document.querySelectorAll('input'),
+	risk_per_trade = document.getElementById('riskPerTrade'),
+	profit = document.getElementById('profit'),
+	loss = document.getElementById('loss'),
+	realized_profit = document.getElementById('realizedprofit'),
+	positionsize = document.getElementById('positionsize')
+
+const numOnly = (e) => {
+	var charCode = e.which ? e.which : e.keyCode
+	if (charCode !== 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+		e.preventDefault()
+	}
+}
+
+const calculate = () => {
+	/* 
+           profit =  pos size * tp
+           loss = pos size * sl
+           risk per trad = margin * capitalrisk
+            realizepnl =  profit - loss
+            pos size = margin * leverage
+    */
+	const rpt = parseInt(margin.value * (capital_risk.value / 100))
+	const posize = parseInt(margin.value * leverage.value)
+	const posizeValue = positionsize.innerText.replace('$', '')
+	const total_profit = parseInt(posizeValue * (takeprofit.value / 100))
+	const total_loss = parseInt(posizeValue * (stoploss.value / 100))
+	const total_pnl = parseInt(total_profit - total_loss)
+	risk_per_trade.innerText = `$${rpt || 0}.00`
+	positionsize.innerText = `$${posize || 0}.00`
+	profit.innerText = `$${total_profit || 0}.00`
+	loss.innerText = `$${total_loss || 0}.00`
+	realized_profit.innerText = `$${total_pnl || 0}.00`
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('input', calculate)
+})
